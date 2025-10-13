4362d7beeb2071430708e6c5a85322e82cfb9825
Tim Feeley <timfee@google.com>
2025-10-13 09:55:27 -0700

    chore: update dependencies and devDependencies in package.json
    feat: remove unused SVG files from public directory
    
    feat: implement NextAuth.js authentication in API route
    
    feat: add drizzle configuration for PostgreSQL database
    
    feat: create environment variable schema using @t3-oss/env-nextjs
    
    feat: set up authentication configuration with DrizzleAdapter and Google provider
    
    feat: establish database connection with drizzle-orm and PostgreSQL
    
    feat: define database schema for users, accounts, sessions, and posts
    
    chore: configure Prettier with Tailwind CSS plugin

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist_Mono, Roboto } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const roboto = Roboto({
+  variable: "--font-roboto",
   subsets: ["latin"],
 });
 
@@ -24,9 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${roboto.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
     </html>
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,10 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path";
+import tseslint from "typescript-eslint";
+import { fileURLToPath } from "url";
+// @ts-ignore -- no types for this plugin
+import drizzle from "eslint-plugin-drizzle";
+import { defineConfig } from "eslint/config";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,6 +14,9 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     ignores: [
@@ -17,9 +24,47 @@ const eslintConfig = [
       ".next/**",
       "out/**",
       "build/**",
+      "*.config.mjs",
+      "*.config.js",
       "next-env.d.ts",
     ],
   },
+  {
+    plugins: {
+      drizzle,
+    },
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: true,
+      },
+    },
+    rules: {
+      "@typescript-eslint/array-type": "off",
+      "@typescript-eslint/consistent-type-definitions": "off",
+      "@typescript-eslint/consistent-type-imports": [
+        "warn",
+        { prefer: "type-imports", fixStyle: "inline-type-imports" },
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/require-await": "off",
+      "@typescript-eslint/no-misused-promises": [
+        "error",
+        { checksVoidReturn: { attributes: false } },
+      ],
+      "drizzle/enforce-delete-with-where": [
+        "error",
+        { drizzleObjectName: ["db", "ctx.db"] },
+      ],
+      "drizzle/enforce-update-with-where": [
+        "error",
+        { drizzleObjectName: ["db", "ctx.db"] },
+      ],
+    },
+  },
 ];
 
-export default eslintConfig;
+export default defineConfig(eslintConfig);
